6fa2a27985647777f24b456c9cb0cbc44049c0ad
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-07-25 11:27:32 +0530

    Time: 0 ms (100%), Space: 9.6 MB (76.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
+      int m = s.size();
+      int n = t.size();
 
-        if(s == t)
-            return true;
-        
+      if(m != n)
         return false;
+    
+    int count[26] = {0};
+    for(int i = 0; i<n; i++){
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
+    }
+    for(int i=0;i<26;i++){
+        if(count[i] != 0){
+            return false;
+        }
+    }
+    
+        return true;
     }
 };
