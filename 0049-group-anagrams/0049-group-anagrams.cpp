5266122758521ea403a6540ae6fc7e65e1b70161
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-07-26 01:25:13 +0530

    Time: 19 ms (44.78%), Space: 25.9 MB (57.17%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        unordered_map<string , vector<string>>mp;
+        vector<vector<string>>result;
+
+        for(int i=0;i<n;i++){
+            string temp = strs[i];
+
+            sort(temp.begin() , temp.end());
+            mp[temp].push_back(strs[i]);
+        }
+
+        for(auto a : mp){
+            result.push_back(a.second);
+        }
+
+        return result;
+
+    }
+};
