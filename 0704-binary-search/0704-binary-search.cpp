e6c2f7d4f2c216f4a366b6a2812ebefc6e75b67b
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-08-02 20:39:29 +0530

    Time: 0 ms (100%), Space: 31.4 MB (8.13%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {            //using reccursion
+    
+    int solve(vector<int>& nums, int l, int r, int target){
+        if(l> r)
+            return -1;
+        int mid = l + (r-l)/2;
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] < target)
+            return solve(nums, mid+1, r, target);
+        else
+            return solve(nums, l, mid-1, target);
+    }
+    
+    
+
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        return solve(nums, 0, n-1, target);
+    }
+};
