a3f5ba32d08eaf75083c70d9c71efd076f0ea221
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-08-06 13:26:41 +0530

    Time: 7 ms (7.02%), Space: 17.7 MB (13.49%) - LeetHub

---
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> minSubsequence(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.rbegin(), nums.rend()); // normal sort tha asc te hoy dsc te orrar jonno rbegin , r end (r= reverse)
+
+        int total_sum = 0;
+        for(int i=0; i<n; i++){
+            total_sum = total_sum + nums[i];
+        }
+
+        vector<int>result;
+        int current_sum = 0;
+        for(int i=0;i<n;i++){
+            current_sum = current_sum + nums[i];
+            result.push_back(nums[i]);
+
+            if (current_sum > (total_sum - current_sum))
+                return result;
+        }
+        return result;
+        
+    }
+};
