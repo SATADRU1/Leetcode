c19e42fed575fc206b4fcf7f639bc0217132a7fb
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-08-09 18:58:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        int n = arr.size();
+        map<int,int>mp;
+        
+        for(int i=0;i<n;i++){
+            int rem = target - arr[i];
+            
+            if(mp.find(rem) != mp.end())
+                return (mp[rem], i);
+            
+            mp[arr[i]] = i;
+        }
+        return {};
+    }
+};
