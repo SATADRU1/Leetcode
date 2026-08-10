efbcb19a7b97885f9c219798eb5c329e589b9674
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-08-10 13:04:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Arrange Anagrams Together/arrange-anagrams-together.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    vector<vector<string>> anagrams(vector<string>& arr) {
+        // code here
+        int n = arr.size();
+        unordered_map<string, vector<string>>mp;
+        vector<vector<string>>result;
+        
+        for(int i=0;i<n;i++){
+            string temp = arr[i];
+            
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(arr[i]);
+            
+        }
+        
+        for(auto a: mp){
+            result.push_back(a.second);
+        }
+        
+        return result;
+    }
+};
