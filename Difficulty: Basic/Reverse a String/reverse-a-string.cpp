f0bb2f0d62f88625561ebc033ed37353e24e527f
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-08-14 13:29:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse a String/reverse-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    string reverseString(string& s) {
+        // code here
+        int n = s.length();
+        int left = 0;
+        int right = n -1;
+        
+        while(left<right){
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+        return s;
+    }
+};
