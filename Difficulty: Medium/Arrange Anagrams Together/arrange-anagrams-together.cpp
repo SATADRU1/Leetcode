0c8c2eed5c7dbd33218a4fd2c941515729c3aa28
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-08-17 18:01:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Arrange Anagrams Together/arrange-anagrams-together.cpp
+++ Difficulty: Medium/Arrange Anagrams Together/arrange-anagrams-together.cpp
@@ -6,15 +6,13 @@ class Solution {
         unordered_map<string, vector<string>>mp;
         vector<vector<string>>result;
         
-        for(int i=0;i<n;i++){
+        for(int i=0; i<n; i++){
             string temp = arr[i];
-            
             sort(temp.begin(), temp.end());
             mp[temp].push_back(arr[i]);
-            
         }
         
-        for(auto a: mp){
+        for(auto a : mp){
             result.push_back(a.second);
         }
         
