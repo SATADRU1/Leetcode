ae671d2129dbca418ef460d760fa6db2a479d703
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-08-18 00:52:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int pos = 0;
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i] != 0)
+                swap(arr[pos++], arr[i]);
+        }
+    }
+};
