6a1e8da545318d585d601b87fbe04cb257149384
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-08-19 21:54:20 +0530

    Time: 0 ms (100%), Space: 10.1 MB (29.29%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>>result;
+    
+    void solve(vector<int>& nums, int i, vector<int>& temp){
+        int n = nums.size();
+        if(i>=n){
+            result.push_back(temp);
+            return ;
+        }
+        //take
+        temp.push_back(nums[i]);
+        solve(nums, i+1, temp);
+        temp.pop_back();
+        solve(nums, i+1, temp);
+
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>temp;
+        solve(nums, 0, temp);
+
+        return result;   //aikhane aktai return upore karon ami to sudhu last node gulo return korbbo gota tree theke 
+    }
+};
