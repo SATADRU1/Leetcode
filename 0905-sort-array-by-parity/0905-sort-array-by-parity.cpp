80ae5586a16b1394ec0537e7e1deaf3503b12324
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-09-05 21:41:47 +0530

    Time: 4 ms (7.16%), Space: 21.8 MB (6.06%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    static bool comparator(int &a, int &b){
+        return a%2 < b%2;
+    }
+
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), comparator);
+        return nums ;
+    }
+};
