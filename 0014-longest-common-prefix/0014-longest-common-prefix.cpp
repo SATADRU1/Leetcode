f09d224d9a4ec3f4f7dd98d1e0671d8ba8093a30
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-09-24 21:53:04 +0530

    Time: 0 ms (100%), Space: 12 MB (39.7%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())
+            return "";
+
+        sort(strs.begin(), strs.end());
+        string first = strs[0];
+        string last = strs.back();
+        string result = "";
+
+        for(int i=0;i< first.length(); i++){
+            if(first[i] != last[i])
+                break;
+            result += first[i];
+        }
+
+        return result;
+    }
+};
